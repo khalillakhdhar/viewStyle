306a743828ce8156de53e594f579fd529fb80cd7
khalil <khalillakhdharatc@gmail.com>
2022-06-27 14:38:14 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {StyleSheet,View, Text} from 'react-native'
+export default class App extends Component {
+render() {
+  //les valeurs fixe (Size) sont toujours prioprietaire
+return ( <View style={styles.container}>
+<View style={{backgroundColor: 'blue', flex: 0.3}} />
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+<View style={{backgroundColor: 'red', flex: 0.5}} />
+
+<Text style={{fontSize: 18}}>View Example</Text>
+</View>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
+const styles = StyleSheet.create ({
+container: {
+flex: 1,
+flexDirection: 'row',
+height: 100,
+width: "80%",
+backgroundColor:"#5ead97"
+} })
